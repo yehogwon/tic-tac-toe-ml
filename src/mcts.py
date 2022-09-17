492419159be359e01f664d1986f3755169f4ff3a
Yeho Gwon <devstita@gmail.com>
2022-09-17 14:42:03 +0900

    changed the way to set a tensor on cuda

--- src/mcts.py
+++ src/mcts.py
@@ -79,7 +79,7 @@ class MCTS:
         while not node.is_leaf(): 
             action, node = node.select(explore)
             state.take_action(action)
-        action_probs, leaf_value = self.network(torch.tensor(state.board, dtype=torch.float32).view(1, 1, 3, 3).to(device))
+        action_probs, leaf_value = self.network(torch.tensor(state.board, dtype=torch.float32, device=device).view(1, 1, 3, 3))
         if state.is_terminal(): 
             winner = state.get_reward()
             if winner == state.turn: 
